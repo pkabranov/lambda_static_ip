f97bfcc5329dc270411fac86ae4479b4e05f06e8
Philip Kabranov <kabranov12@gmail.com>
2024-08-23 10:39:40 -0700

    added comments to explain each block

--- main.tf
+++ main.tf
@@ -12,15 +12,18 @@ provider "aws" {
 
 }
 
+# Pet name for S3 bucket
 resource "random_pet" "lambda_bucket_name" {
   prefix = "learn-terraform-functions"
   length = 4
 }
 
+# S3 bucket using generated pet name
 resource "aws_s3_bucket" "lambda_bucket" {
   bucket = random_pet.lambda_bucket_name.id
 }
 
+# 
 resource "aws_s3_bucket_ownership_controls" "lambda_bucket" {
   bucket = aws_s3_bucket.lambda_bucket.id
   rule {
@@ -28,9 +31,28 @@ resource "aws_s3_bucket_ownership_controls" "lambda_bucket" {
   }
 }
 
+# S3 access control list (set to private)
 resource "aws_s3_bucket_acl" "lambda_bucket" {
   depends_on = [aws_s3_bucket_ownership_controls.lambda_bucket]
 
   bucket = aws_s3_bucket.lambda_bucket.id
   acl    = "private"
+}
+
+# Zip file to store lambda code
+data "archive_file" "lambda_hello_world" {
+  type = "zip"
+
+  source_dir  = "${path.module}/hello-world"
+  output_path = "${path.module}/hello-world.zip"
+}
+
+# S3 object that consists of the zipped lambda code
+resource "aws_s3_object" "lambda_hello_world" {
+  bucket = aws_s3_bucket.lambda_bucket.id
+
+  key    = "hello-world.zip"
+  source = data.archive_file.lambda_hello_world.output_path
+
+  etag = filemd5(data.archive_file.lambda_hello_world.output_path)
 }
